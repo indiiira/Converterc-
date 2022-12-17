74e500dd2a9019a602f5ce79da9a6211bdfc7f6f
indiiira <indira_shakirova2003@mail.ru>
2022-12-17 15:09:42 +0300

    изменен дизайн

--- Converterc++/Form1.h
+++ Converterc++/Form1.h
@@ -52,6 +52,7 @@ namespace CppCLRWinformsProjekt {
 
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
 
 	private:
 		/// <summary>
@@ -74,26 +75,27 @@ namespace CppCLRWinformsProjekt {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Mongolian Baiti", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
 			this->label1->Location = System::Drawing::Point(27, 101);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(294, 30);
+			this->label1->Size = System::Drawing::Size(268, 28);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"Введите сумму в рублях";
+			this->label1->Text = L"Введите сумму в рублях:";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(172, 9);
+			this->label2->Location = System::Drawing::Point(238, 9);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(174, 24);
 			this->label2->TabIndex = 1;
@@ -107,7 +109,7 @@ namespace CppCLRWinformsProjekt {
 			this->comboBox1->ForeColor = System::Drawing::Color::Black;
 			this->comboBox1->FormattingEnabled = true;
 			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"BYN", L"EUR", L"KZT", L"USD" });
-			this->comboBox1->Location = System::Drawing::Point(176, 55);
+			this->comboBox1->Location = System::Drawing::Point(242, 56);
 			this->comboBox1->Name = L"comboBox1";
 			this->comboBox1->Size = System::Drawing::Size(170, 24);
 			this->comboBox1->TabIndex = 2;
@@ -118,11 +120,11 @@ namespace CppCLRWinformsProjekt {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(54, 166);
+			this->label3->Location = System::Drawing::Point(27, 166);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(60, 28);
+			this->label3->Size = System::Drawing::Size(248, 28);
 			this->label3->TabIndex = 3;
-			this->label3->Text = L"Курс";
+			this->label3->Text = L"Информация про курс";
 			// 
 			// button1
 			// 
@@ -142,7 +144,7 @@ namespace CppCLRWinformsProjekt {
 			this->textBox1->BackColor = System::Drawing::Color::MediumTurquoise;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->textBox1->Location = System::Drawing::Point(349, 101);
+			this->textBox1->Location = System::Drawing::Point(411, 101);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(192, 26);
 			this->textBox1->TabIndex = 5;
@@ -152,7 +154,7 @@ namespace CppCLRWinformsProjekt {
 			this->button2->BackColor = System::Drawing::Color::PaleTurquoise;
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->button2->Location = System::Drawing::Point(334, 242);
+			this->button2->Location = System::Drawing::Point(464, 242);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(207, 62);
 			this->button2->TabIndex = 7;
@@ -165,18 +167,30 @@ namespace CppCLRWinformsProjekt {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(353, 166);
+			this->label4->Location = System::Drawing::Point(576, 169);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(27, 25);
 			this->label4->TabIndex = 8;
 			this->label4->Text = L"...";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(404, 169);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(112, 28);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Результат";
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::Teal;
-			this->ClientSize = System::Drawing::Size(570, 327);
+			this->ClientSize = System::Drawing::Size(705, 327);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox1);
@@ -220,5 +234,6 @@ namespace CppCLRWinformsProjekt {
 
 
 
-	};
+	
+};
 }
